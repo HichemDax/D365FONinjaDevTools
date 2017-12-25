0ae9659a52549c2cfee0dd374505d3bd4f5c5a36
Hichem CHEKEBKEB <hichem.dax@outlook.com>
2017-12-24 20:37:55 -0800

    Cleanups

--- SetupD365FOAddins/Program.cs
+++ SetupD365FOAddins/Program.cs
@@ -19,6 +19,7 @@ namespace SetupD365FOAddins
                         , true);
 
 
+
                 Console.WriteLine("Setup finished! Close and enjoy!");
                 Console.ReadLine();
             
